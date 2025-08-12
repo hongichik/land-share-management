e3cc8faefcfb1da76a83ef8a98022031483c0306
hongchik <hongichik321@gmail.com>
2025-08-12 11:37:04 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,3 @@ Route::get('/', function () {
     return redirect()->route('admin.dashboard');
 });
 
-// Thêm route cho việc xuất kế hoạch thuê đất
-Route::get('/admin/land-rental-contracts/export-plan', [App\Http\Controllers\Admin\LandRentalContractController::class, 'exportRentalPlan'])
-    ->name('admin.land-rental-contracts.export-plan');
-
-    
-// Catch-all route: redirect all other URLs to admin.dashboard
-
